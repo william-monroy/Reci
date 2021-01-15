017ec01b3f603f7d30283c9ac84c1749c3391d8a
william-monroy <william_monroy777@hotmail.com>
2021-01-15 17:32:16 -0500

    estilo y funciones

--- js/index.js
+++ js/index.js
@@ -1,53 +1,44 @@
-var info;
-
-
-
 // Initialize and add the map
 function initMap() {
-    /* The location of Uluru
-    const uluru = {
-        lat: -25.344,
-        lng: 131.036
-        
-    };*/
     
-    d3.json("data.json", function (error, data) {
+    d3.json("data.json", function (error, resultado) {
         if (error){
             throw error;
         }
-        this.data = data;
-        console.log(data);
-    });
-
-    var geo = [
-        {
-            lat: 25.652141088781722, 
-            lng: -100.29682220205787
-        },
-        {
-            lat: 25.646724987221507, 
-            lng: -100.32548965119364
-        },
-        {
-            lat: 25.656473793012132, 
-            lng: -100.2757078532932
-        },
-        {
-            lat: 25.654008470930716, 
-            lng: -100.29223323721736
+        console.log(resultado);
+        
+        const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 13,
+            center: resultado[0] 
+        });
+        
+        for (let i = 0; i < resultado.length; i++) {
+            var maker = new google.maps.Marker({
+                position: resultado[i],
+                map: map,
+            })
         }
-    ];
-
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
-        center: geo[0],
+        
     });
     
-    for (let i = 0; i < geo.length; i++) {
-        var maker = new google.maps.Marker({
-            position: geo[i],
-            map: map,
-        })
+}
+
+function crear() {
+    const latitud = parseInt(document.getElementById('lat').value);
+    const longitud = parseInt(document.getElementById('long').value);
+    if (latitud == '' || longitud == '') {
+        alert('Introduzca datos validos')
+    }else{
+        console.log('latitud:' + latitud);
+        console.log('longitud:' + longitud);
+
+        d3.json("data.json", function (error, data) {
+            if (error){
+                throw error;
+            }
+            this.data = data;
+            console.log(data);
+        });
     }
+    // 25.6783295515985, -100.3140002911494
 }
